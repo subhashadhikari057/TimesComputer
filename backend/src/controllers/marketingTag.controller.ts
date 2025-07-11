31a3576b2c9e60aadb1b4ed1b55624636f2221a8
Nabin Poudel <nabinpoudel788@gmail.com>
2025-07-12 00:03:51 +0545

    feat(controller): add marketing tag CRUD and related product fetching

---
+++ backend/src/controllers/marketingTag.controller.ts
@@ -0,0 +1,111 @@
+import { Request, Response } from "express";
+import prisma from "../prisma/client";
+
+// Create a new Marketing-Tag
+export const addMarketingTag = async (req: Request, res: Response) => {
+    try {
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(400).json({ error: "MarketingTag name is required." });
+        }
+
+        const MarketingTag = await prisma.marketingTag.create({
+            data: { name, },
+        });
+
+        res.status(201).json({ message: "MarketingTag created successfully.", data: MarketingTag });
+    } catch (error: any) {
+        console.error("Create MarketingTag error:", error);
+        res.status(500).json({ error: "Internal server error." });
+    }
+};
+
+// Get all Marketing-Tags
+export const getAllMarketingTags = async (_req: Request, res: Response) => {
+    try {
+        const MarketingTags = await prisma.marketingTag.findMany();
+        res.status(200).json({ message: "MarketingTags retrieved successfully.", data: MarketingTags });
+    } catch (error: any) {
+        console.error("Get all MarketingTags error:", error);
+        res.status(500).json({ error: "Failed to retrieve MarketingTags." });
+    }
+};
+
+// Get a single Marketing-Tag by ID
+export const getMarketingTagById = async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) return res.status(400).json({ error: "Invalid MarketingTag ID." });
+
+        const MarketingTag = await prisma.marketingTag.findUnique({ where: { id } });
+
+        if (!MarketingTag) return res.status(404).json({ error: "MarketingTag not found." });
+
+        res.status(200).json({ message: "MarketingTag retrieved successfully.", data: MarketingTag });
+    } catch (error: any) {
+        console.error("Get MarketingTag by ID error:", error);
+        res.status(500).json({ error: "Failed to retrieve MarketingTag." });
+    }
+};
+
+// Update a Marketing-Tag
+export const updateMarketingTag = async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id);
+        const { name } = req.body;
+
+        if (isNaN(id)) return res.status(400).json({ error: "Invalid MarketingTag ID." });
+
+        if (!name || typeof name !== "string") {
+            return res.status(400).json({ error: "MarketingTag name is required and must be a string." });
+        }
+
+        const MarketingTag = await prisma.marketingTag.findUnique({ where: { id } });
+        if (!MarketingTag) return res.status(404).json({ error: "MarketingTag not found." });
+
+        const updatedMarketingTag = await prisma.marketingTag.update({
+            where: { id },
+            data: { name },
+        });
+
+        res.status(200).json({ message: "MarketingTag updated successfully.", data: updatedMarketingTag });
+    } catch (error: any) {
+        console.error("Update MarketingTag error:", error);
+        res.status(500).json({ error: "Failed to update MarketingTag." });
+    }
+};
+
+// Delete Marketing-Tag
+export const deleteMarketingTag = async (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        if (isNaN(id)) return res.status(400).json({ error: "Invalid MarketingTag ID." });
+
+        const MarketingTag = await prisma.marketingTag.findUnique({ where: { id } });
+        if (!MarketingTag) return res.status(404).json({ error: "MarketingTag not found." });
+
+        const deleteMarketingTag = await prisma.marketingTag.delete({
+            where: { id }
+        });
+
+        res.status(200).json({ message: "MarketingTag deleted successfully.", deleteMarketingTag });
+    } catch (error: any) {
+        console.error("Delete MarketingTag error:", error);
+        res.status(500).json({ error: "Failed to delete MarketingTag." });
+    }
+}
+
+//  Get Product linked to a Marketing Tag
+export const getProductByMarketingTag = async (req: Request, res: Response) => {
+    try {
+        const tagId = parseInt(req.params.id);
+
+        const product = await prisma.productMarketingTag.findMany({ where: { tagId }, include: { product: true } });
+        const productList = product.map(pft => pft.product);
+        res.status(200).json({ productList });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+}
