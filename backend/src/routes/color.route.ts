5c7c4e67d235172fc12e05fbb60b303353043538
Nabin Poudel <nabinpoudel788@gmail.com>
2025-07-12 00:42:53 +0545

    feat(routes): define CRUD rutes for colors.

---
+++ backend/src/routes/color.route.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { addColor, deleteColor, getAllColors, getColorById, getProductByColor, updateColor } from "../controllers/color.controller";
+import { authenticate, isAdmin } from "../middlewares/auth.middleware";
+
+const router = express.Router();
+
+router.route("/add").post(authenticate, isAdmin, addColor);
+router.route("/get").get(getAllColors);
+router.route("/get/:id").get(getColorById);
+router.route("/get/:id/products").get(getProductByColor);
+router.route("/update/:id").patch(authenticate, isAdmin, updateColor);
+router.route("/delete/:id").delete(authenticate, isAdmin, deleteColor);
+
+export default router;
