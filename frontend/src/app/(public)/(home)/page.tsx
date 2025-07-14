c17bb3cdbcb0140285895503e39b955abaf5cd61
Subhash Adhikari <subhashadhikari057@gmail.com>
2025-07-14 14:12:26 +0545

    Added brand scroller

--- frontend/src/app/(public)/(home)/page.tsx
+++ frontend/src/app/(public)/(home)/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import BannerSection from '@/components/banner/bannerSection'
 import { FeaturedProductsSection } from '@/components/products/productsection'
+import BrandScroller from '@/components/home/BrandScroller';
+
 const HomePage = () => {
   return (
-   <div className="max-w-[1920px] mx-auto">
-    <BannerSection />
-    <FeaturedProductsSection />
-   </div>
+    <div className="max-w-[1920px] mx-auto">
+      <BannerSection />
+      <FeaturedProductsSection />
+      <BrandScroller />
+    </div>
   )
 };
 export default HomePage;
