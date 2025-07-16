0a4751c924a314024124cef92f1319095bf5c825
Nabin Poudel <nabinpoudel788@gmail.com>
2025-07-16 16:55:01 +0545

    feat(ads): redefine ads CRUD routes in chain method

--- backend/src/routes/ads.route.ts
+++ backend/src/routes/ads.route.ts
@@ -1,14 +1,11 @@
 import express from "express";
-import { uploadImage } from '../middlewares/upload.middleware';
-import { authenticate, isAdmin, isSuperadmin } from "../middlewares/auth.middleware";
-import { addAds, deleteAds, getAdsById, getAllAds, updateAds } from "../controllers/adBanner.controller";
-
 const router = express.Router();
 
-router.route("/add").post(authenticate, isSuperadmin, isAdmin, uploadImage, addAds);
-router.route("/get").get(getAllAds);
-router.route("/get/:id").get(getAdsById);
-router.route("/update/:id").patch(authenticate, isAdmin, updateAds);
-router.route("/delete/:id").delete(authenticate, isAdmin, deleteAds);
+import { uploadImages } from '../middlewares/upload.middleware';
+import { authenticate, isAdmin } from "../middlewares/auth.middleware";
+import { addAds, deleteAds, getAdsById, getAllAds, updateAds } from "../controllers/adBanner.controller";
+
+router.route("/").get(getAllAds).post(authenticate, isAdmin, uploadImages, addAds);
+router.route("/:id").get(getAdsById).patch(authenticate, isAdmin, uploadImages, updateAds).delete(authenticate, isAdmin, deleteAds).patch(authenticate, isAdmin, uploadImages, updateAds);
 
 export default router;
