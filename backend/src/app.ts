a083a2a3e7ea2af6a220bec40b03e0f77c441254
Nabin Poudel <nabinpoudel788@gmail.com>
2025-07-16 17:05:40 +0545

    Import ads and blog route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,14 +9,16 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route';
 import adminUserRoutes from "./routes/adminUser.route";
-import categoryRoutes from "./routes/category.route";
+import adsRoutes from "./routes/ads.route";
+import blogRoutes from "./routes/blog.route";
 import brandRoutes from "./routes/brand.route";
-import featureTagRoutes from "./routes/featureTag.route";
-import marketingTagRoutes from "./routes/marketingTags.route";
 import colorRoutes from "./routes/color.route";
 import productRoutes from "./routes/product.route";
 import inquiryRoutes from "./routes/inquiry.route";
-import adRoutes from "./routes/ads.route";
+import categoryRoutes from "./routes/category.route";
+import featureTagRoutes from "./routes/featureTag.route";
+import marketingTagRoutes from "./routes/marketingTags.route";
+
 
 
 dotenv.config();
@@ -48,14 +50,15 @@ app.get('/api/csrf-token', (req, res) => {
 // ✅ Routes
 app.use('/api/auth', authRoutes);
 app.use("/admin/users", adminUserRoutes);
-app.use("/api/category", categoryRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/ads", adsRoutes);
+app.use("/api/blog", blogRoutes);
 app.use("/api/brand", brandRoutes);
+app.use("/api/colors", colorRoutes);
+app.use("/api/inquiry", inquiryRoutes);
+app.use("/api/category", categoryRoutes);
 app.use("/api/feature-tag", featureTagRoutes);
 app.use("/api/marketing-tag", marketingTagRoutes);
-app.use("/api/colors", colorRoutes);
-app.use("/api/product", productRoutes);
-app.use("/api/inquiry", inquiryRoutes);
-app.use("/api/ads", adRoutes);
 
 
 // ✅ Health check
