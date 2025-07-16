39c421753bd88c9584b3071bb2d37ae3e06a0a62
Nabin Poudel <nabinpoudel788@gmail.com>
2025-07-16 16:56:04 +0545

    feat(featureTag): redefine featureTag CRUD routes in chain method

--- backend/src/routes/featureTag.route.ts
+++ backend/src/routes/featureTag.route.ts
@@ -1,14 +1,11 @@
 import express from "express";
-import { addFeatureTag, deleteFeatureTag, getAllFeatureTag, getFeatureTagById, getProductByFeatureTag, updateFeatureTag } from "../controllers/featureTag.controller";
-import { authenticate, isAdmin } from "../middlewares/auth.middleware";
-
 const router = express.Router();
 
-router.route("/add").post(authenticate, isAdmin, addFeatureTag);
-router.route("/get").get(getAllFeatureTag);
-router.route("/get/:id").get(getFeatureTagById);
-router.route("/get/:id/products").get(getProductByFeatureTag);
-router.route("/update/:id").patch(authenticate, isAdmin, updateFeatureTag);
-router.route("/delete/:id").delete(authenticate, isAdmin, deleteFeatureTag);
+import { authenticate, isAdmin } from "../middlewares/auth.middleware";
+import { addFeatureTag, deleteFeatureTag, getAllFeatureTag, getFeatureTagById, getProductByFeatureTag, updateFeatureTag } from "../controllers/featureTag.controller";
+
+router.route("/products/:id").get(getProductByFeatureTag);
+router.route("/").get(getAllFeatureTag).post(authenticate, isAdmin, addFeatureTag);
+router.route("/:id").get(getFeatureTagById).patch(authenticate, isAdmin, updateFeatureTag).delete(authenticate, isAdmin, deleteFeatureTag);
 
 export default router;
