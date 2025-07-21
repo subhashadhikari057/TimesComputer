29124204448a92dadb61f8a0297d2adcf78286e5
PERIS <periskc@gmail.com>
2025-07-21 16:52:37 +0545

    New spinner

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { Spinner } from '@geist-ui/core';
 
 export default function AdminPage() {
   const router = useRouter();
@@ -14,5 +15,9 @@ export default function AdminPage() {
     }
   }, [router]);
 
-  return null; // or a loading spinner
+  return (
+    <div className='h-screen grid place-content-center'>
+      <Spinner scale={5} />
+    </div>
+  );
 }
